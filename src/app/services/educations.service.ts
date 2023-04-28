44fd252abdbbcbf17ad1c1f420106adce3fd3f02
EzequielDalzotto <dalzottoezequiel@gmail.com>
2023-04-28 02:51:53 -0300

    educacion interface & service

--- src/app/services/educations.service.ts
+++ src/app/services/educations.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Educacion } from '../model/educacion';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class EducationsService {
+  private apiUrl = 'http://localhost:5000/educaciones'
 
-  constructor() { }
+
+  constructor(private http:HttpClient) { }
+
+  getEducations():Observable<Educacion[]>{
+    return this.http.get<Educacion[]>(this.apiUrl)
+  }
 }
