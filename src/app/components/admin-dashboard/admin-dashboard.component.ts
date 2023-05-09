c36532b09ef8a0af9cbdf1fe30cc49512749c745
EzequielDalzotto <dalzottoezequiel@gmail.com>
2023-05-09 04:02:25 -0300

    dashboard left column dynamic

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AboutMeService } from 'src/app/services/about-me.service';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.css']
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit {
+  about_me: any = {};
 
+  constructor(private aboutMeService: AboutMeService) {  }
+
+  ngOnInit(): void {
+      this.aboutMeService.getAboutMe().subscribe((about_me)=>[
+        this.about_me = about_me
+      ])
+  }
 }
